d2ca77b1caa8d3b7b835355a8db0807ffa574a9b
Minami <minamikanesawa@gmail.com>
2025-03-04 17:34:57 +0100

    add a journal entry

--- src/app/_components/JournalEntries.tsx
+++ src/app/_components/JournalEntries.tsx
@@ -7,6 +7,10 @@ type JournalEntry = {
 
 const entries: JournalEntry[] = [
   {
+    date: "2025-03-04",
+    text: "Tested in the production environment, and it worked fine. Fixed sanity hydration issue.",
+  },
+  {
     date: "2025-03-03",
     text: "Tried to solve second load issue (not because of React Strict Mode), but it was not successful. I don't not why it's redering twice otherwise...",
   },
