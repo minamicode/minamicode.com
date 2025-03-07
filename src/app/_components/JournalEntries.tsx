c472f0bbd1e3c6e8a34f3bedff4e75f17c21b70d
Minami <minamikanesawa@gmail.com>
2025-03-07 23:12:53 +0100

    add a journal entry

--- src/app/_components/JournalEntries.tsx
+++ src/app/_components/JournalEntries.tsx
@@ -7,6 +7,10 @@ type JournalEntry = {
 
 const entries: JournalEntry[] = [
   {
+    date: "2025-03-07",
+    text: "Improved UI, fixed country selector location mismatch due to race condition and improved overall performance.",
+  },
+  {
     date: "2025-03-06",
     text: "Improved UI of tinder style wedding vendor directory for mobile.",
   },
